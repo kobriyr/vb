e7daebeafe680b737e87ee9e81737993da33b374
roman <68932dh_!aT>
2021-11-02 23:09:22 +0200

    Added new 9 volume

--- public/polyfills.js
+++ public/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/roman/Documents/Projects/vb_ui/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/roman/Documents/Projects/blagovisnik/vb_ui/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
